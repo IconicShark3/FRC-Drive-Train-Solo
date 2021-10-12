e37fefadc224756fbb1a38b97ba35054a168ebc8
Colin Mills <colin.lego.scout@gmail.com>
2021-10-12 16:27:41 -0700

    it should stop now

--- FRC Drive Train SOLO/src/main/java/frc/robot/commands/AutoDrive.java
+++ FRC Drive Train SOLO/src/main/java/frc/robot/commands/AutoDrive.java
@@ -6,13 +6,15 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /** An example command that uses an example subsystem. */
 public class AutoDrive extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final DriveTrainSubsystem m_subsystem;
-
+  private final Timer m_timer = new edu.wpi.first.wpilibj.Timer();
   /**
    * Creates a new AutoDrive .
    *
@@ -26,24 +28,29 @@ public class AutoDrive extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
+  public void initialize() {
+    m_timer.start(); 
+  }
+    
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     m_subsystem.arcadeDrive(0.5, 0);
+    
     //How this works I do not know but it might work
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_timer.get()>1;
   }
 }
 
